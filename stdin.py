9ec4b8b89fd9d9d551c2e09322c4cc8bf9c4372b
ravir1981 <ravir1981@yahoo.com>
2024-01-05 13:52:53 -0500

    added length function

--- stdin.py
+++ stdin.py
@@ -3,6 +3,16 @@ todo1 = input(get_input)
 todo2 = input(get_input)
 todo3 = input(get_input)
 
-todos = [todo1, todo2, todo3]
+todos = [todo1, todo2, todo3,'Hello']
 
 print(todos)
+
+# To find type of variable
+print(type(todo1))
+print(type(todos))
+
+text = input("Enter the String:")
+
+length = len(text)
+
+print("Length of the string is :", length)
